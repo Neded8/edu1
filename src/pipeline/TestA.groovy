385eb6e1e2cf3a71887b96ec94a5bb2ae44390e3
vladislavo <vlad.opanasiuk.99@gmail.com>
2021-09-13 13:04:05 +0300

    dfff

--- src/pipeline/TestA.groovy
+++ src/pipeline/TestA.groovy
@@ -1,25 +1,21 @@
 package pipeline
 
 public void test(){
-    pipeline {
-        agent none
-        stages {
-            stage('Build') {
-                steps {
-                    echo 'Buils Stage'
-                }
-            }
-            stage('Test') {
-                steps {
-                    echo 'Test stage'
-                }
-            }
-            stage('Deploy') {
-                steps {
-                    echo 'Deploy stage'
-                }
-            }
+    stage('Build') {
+        steps {
+            echo 'Buils Stage'
         }
     }
-
+    stage('Test') {
+        steps {
+            echo 'Test stage'
+        }
+    }
+    stage('Deploy') {
+        steps {
+            echo 'Deploy stage'
+        }
+    }
+}
 return this
+
