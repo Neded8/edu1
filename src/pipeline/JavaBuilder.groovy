fbca6172d8f82971b0ea59e6426b8f572fb8dcd8
vladislavo <vlad.opanasiuk.99@gmail.com>
2021-09-13 15:11:40 +0300

    normal version 8

--- src/pipeline/JavaBuilder.groovy
+++ src/pipeline/JavaBuilder.groovy
@@ -1,8 +1,48 @@
 package pipeline
 
-private void cleanUp(){
-    echo "delete current directory"
-    deleteDir()
+private void cleanUp() {
+    stage("clean home directory") {
+        echo "delete current directory"
+        deleteDir()
+    }
+}
+
+private void getSourceCode(String repoURL, String branchName) {
+    stage("checkout to git") {
+        checkout changelog: true, poll: true, scm: [
+                $class                           : 'GitSCM',
+                branches                         : [[name: branchName]],
+                doGenerateSubmoduleConfigurations: false,
+                userRemoteConfigs                : [[url: repoURL]]]
+    }
+
+}
+
+private void runMaven(command) {
+    if (isUnix()) {
+//        withEnv(["JAVA_HOME=OLOLOL"]){
+        sh "mvn ${command}"
+//        }
+
+    } else {
+//        withEnv(["JAVA_HOME=OLOLOL"]){
+        bat "mvn ${command}"
+//        }
+    }
+}
+
+private void buildMaven() {
+    stage("build") {
+        runMaven("clean package")
+    }
+}
+
+void run(String nodeName, String repoURL, String branchName) {
+    node(nodeName) {
+        cleanUp()
+        getSourceCode(repoURL, branchName)
+        buildMaven()
+    }
 
 }
 
