7a121ccda92b74d4e9b91e6b70661d80a0e7e45c
vladislavo <vlad.opanasiuk.99@gmail.com>
2021-09-13 13:09:55 +0300

    normal version 2

--- src/pipeline/TestA.groovy
+++ src/pipeline/TestA.groovy
@@ -1,20 +1,19 @@
 package pipeline
 
 public void test() {
-    pipeline {
-        agent any
-        stages {
-            stage('Build') {
-                echo "Build stage"
-            }
-            stage('Test') {
-                echo "Test stage"
-            }
-            stage('Deploy') {
-                echo "Deploy stage"
-            }
+    agent any
+    stages {
+        stage('Build') {
+            echo "Build stage"
+        }
+        stage('Test') {
+            echo "Test stage"
+        }
+        stage('Deploy') {
+            echo "Deploy stage"
         }
     }
 }
+
 return this
 
